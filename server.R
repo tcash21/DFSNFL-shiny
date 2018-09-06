6569775d72dfd9e889d675ce75de8a09b42ed3ac
root <root@ip-10-154-79-211.ec2.internal>
2018-09-05 23:02:07 -0400

    2018 season

--- server.R
+++ server.R
@@ -19,16 +19,13 @@ refresh_data <- reactive({
 
 filterData <- reactive({
  	
-	#preds <- reactiveValues()
-	#preds$df <-  read.csv("http://s3.amazonaws.com/tcbdfs/preds_nfl.csv")
-
-	preds$df$name <- as.character(preds$df$player)
- 	preds$df$team <-as.character(preds$df$Team)
-        preds$df$Position <- as.character(preds$df$Position)
+	preds$df$player_names <- as.character(preds$df$player_names)
+ 	preds$df$team <-as.character(preds$df$teams)
+        preds$df$positions <- as.character(preds$df$positions)
 	preds$df$title <- as.character(preds$df$title)
 	preds2 <- preds$df
 	preds2 <- preds2[preds2$line >= input$line,]
-	preds2 <- preds2[preds2$value <= input$value,]
+#	preds2 <- preds2[preds2$value <= input$value,]
 	preds2 <- preds2[preds2$Salary >= input$salary[1] & preds2$Salary <= input$salary[2],]
 	preds2 <- preds2[preds2$title %in% input$games,]
 	preds2$spread <- preds2$spread * -1
@@ -37,31 +34,29 @@ filterData <- reactive({
 	}
 	
 	
-	if(input$type == 'NF'){
-		obj <- preds2$nf_pred
-	} else {
-		obj <- preds2$score
-	}
-	con <- rbind(t(model.matrix(~ Position + 0,preds2)), t(model.matrix(~ team + 0, preds2)), rep(1,nrow(preds2)), preds2$Salary)
-	dir <- c("=","=","=","=","=", "=", rep('<=',length(unique(preds2$team))),"=","<=")
-	rhs <- c(1,1,1,2,1,3,rep(4,length(unique(preds2$team))),9,60000)
+#	if(input$type == 'NF'){
+#		obj <- preds2$nf_pred
+#	} else {
+#		obj <- preds2$score
+#	}
+
+	obj <- preds2$FP
+	con <- rbind(t(model.matrix(~ positions + 0,preds2)), t(model.matrix(~ team + 0, preds2)), rep(1,nrow(preds2)), preds2$Salary)
+	dir <- c("=","=","<=","<=","<=", rep('<=',length(unique(preds2$team))),"=","<=")
+	rhs <- c(1,1,3,2,4,rep(4,length(unique(preds2$team))),9,60000)
 	result <- lp("max", obj, con, dir, rhs, all.bin = TRUE)	
 	results <- preds2[which(result$solution == 1),]
 	results$spread <- as.numeric(results$spread)
 	#results$spread <- results$spread * -1
-	results <- results[,c("name", "Position", "team","Opponent.x", "Salary", "nf_pred","line", "spread", "pa", "value")]
-	
+	results <- results[,c("player_names", "positions", "team", "opponent", "Salary", "FP","line", "spread", "pa", "value")]
+	colnames(results) <- c("Player", "Position", "Team", "Opp", "Salary", "FP", "Line", "Spread", "PA", "Value")
 	return(list(results, result))
    
 })
 
-deleteData <- function(rowID){
-	preds$df <- preds$df[-match(rowID, rownames(preds$df)),]
-}
-
-
 deleteRow <- observeEvent(input$delete.button, {
-	deleteData(input$results_rows_selected)
+	chosen_player <- preds$df[c(input$results_rows_selected),]$player_names
+        preds$df <- preds$df[-which(preds$df$player_names %in% chosen_player),]
 })
 
 refresh <- observeEvent(input$refresh, {
@@ -69,7 +64,7 @@ refresh <- observeEvent(input$refresh, {
 })
 
 output$results <- renderDataTable({
-	datatable(filterData()[[1]], options=list(autowidth=TRUE, selection="single", columnDefs = list(list(width='200px', targets="_all"))), rownames=TRUE, escape = FALSE)
+	datatable(filterData()[[1]], options=list(autowidth=TRUE, selection="single", columnDefs = list(list(width='200px', targets="_all"))), rownames=FALSE, escape = FALSE)
 })
 
 output$update <- renderPrint({
@@ -82,6 +77,12 @@ output$update <- renderPrint({
 	cat(as.character(HTML(last_update)))
 })
 
+output$allplayers <- renderDataTable({
+	preds3 <- preds$df
+	preds3$spread <- preds3$spread * -1
+	datatable(preds3, options=list(autowidth=TRUE))
+})
+
 output$total <- renderPrint({
   r <- filterData()[[2]]
   
@@ -91,17 +92,24 @@ output$total <- renderPrint({
 
 output$about <- renderPrint({
 	info <- "This optimizer generates optimal lineups based on NumberFire projections:<p>
-		<ul><li><b>numberFire</b>:  <a href = 'https://www.numberfire.com/nba/daily-fantasy/daily-baseball-projections/'>NumberFire.com</a> projections</li>
+		<ul><li><b>numberFire</b>:  <a href = 'https://www.numberfire.com/nfl/fantasy/fantasy-football-projections'>NumberFire.com</a> projections</li>
 		<p>
-		You can filter by games, salary and value. You can also exclude and lock certain players. Data is updated every 30 minutes.
+		You can filter by games, salary and value. You can exclude certain players. You can also select 'Favorites' only,
+		which will only allow players on teams that are favored to win according to Vegas. Data is updated every 30 minutes.
 		<p>
-		Gam schedule data powered by  <a href = 'https://www.stattleship.com' target='_blank'>Stattleship</a>.
+		Legend
+		<ul><li><b>NF</b> - NumberFire Projections</li>
+			<li><b>TCB</b> - Proprietary TCB Optimization</li>
+			<li><b>pa</b> - average Fanduel points against that position</li>
+		</ul>
+		Game schedule data powered by  <a href = 'https://www.stattleship.com' target='_blank'>Stattleship</a>.
 		</p>
 		To take advantage of this tool, sign up for <a href = 'https://www.fanduel.com/?invitedby=tcash21&cnl=da' target='_blank'>FanDuel</a> to start playing today."
 	cat(as.character(HTML(info)))
 })
 
 })
+
 
 
 
--- ui.R
+++ ui.R
@@ -12,6 +12,9 @@ preds$title <- as.character(preds$title)
 if(length(which(is.na(preds$line))) > 0){
 	preds <- preds[-which(is.na(preds$line)),]
 }
+if(length(which(is.na(preds$spread))) > 0){
+        preds <- preds[-which(is.na(preds$spread)),]
+}
 
 
 shinyUI(pageWithSidebar(
@@ -22,8 +25,8 @@ shinyUI(pageWithSidebar(
      a(img(src="logo.png", align="center", width="170px", class="nav_logo"), href="http://tcbanalytics.com", target="_blank"),
      includeCSS('www/app.css'),
      numericInput("line", "Lines >= :", value=min(preds$line), min=min(preds$line),  max=max(preds$line)),
-     sliderInput("value", "Value >= :", value=max(preds$value), min=min(preds$value), max=1000),
-     selectInput("type", "Objective", choices=c("TCB", "NF"), selected="TCB"),
+     #sliderInput("value", "Value >= :", value=max(preds$value), min=min(preds$value), max=max(preds$value)),
+     selectInput("type", "Objective", choices=c("NF"), selected="NF"),
      sliderInput("salary", "Salary >= :", value=c(min(preds$Salary), max(preds$Salary)), min=min(preds$Salary), max=max(preds$Salary)),
      checkboxInput("fav", "Favorites Only:", value = FALSE),
      checkboxGroupInput("games", "Games:", ug$title, selected=ug$title),
@@ -40,6 +43,7 @@ shinyUI(pageWithSidebar(
 			column(6, dataTableOutput('results')), 
 			column(5, htmlOutput("update"))
 		),
+		tabPanel("All Players",dataTableOutput('allplayers')),
 		tabPanel("Help", htmlOutput("about"))
 	)
 #     dataTableOutput('results')
@@ -48,3 +52,4 @@ shinyUI(pageWithSidebar(
     )
     
 ))
+
